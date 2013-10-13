84026fa3375ac6e49bd39917ec9dc0f166791051
Thomas Powell <tpowell@appriss.com>
2013-10-13 10:16:48 -0400

    Initial rails project with rspec and cucumber installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LottoPiggyBank::Application.config.secret_token = '53bd3ceaeedd882c0e687bc3f2cb02461acfa1844ae08e63b37cef02c336b9b5936afe48f472e2b9dafbfb7a4e186a7874bf1d9996f178f58e5fd1cf4beffa4b'
